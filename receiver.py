3b39e94fcd612da74e47fccf422f2a4d2a193484
Rafael Fernandes <faelsfernandes@gmail.com>
2019-10-24 12:28:04 -0300

    Parser adicionado

--- receiver.py
+++ receiver.py
@@ -1,21 +1,44 @@
 import socket
 import time 
 import random 
-dest = ('127.0.0.1', 9091)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(dest)
-while (True):
-    data, adress = server_socket.recvfrom(512)
-    if (data):
-        # print (data)
-        number = random.randint(0,20)
-        if(number < 10):
-            time.sleep(0.5)
-            print("Received \'" + str(data) + "\', but NO send AKC")
-        else:
-            server_socket.sendto(b'AKC', adress)
-            print("Received \'" + str(data) + "\' with AKC")
-        data = ''
-    else:
-        server_socket.close()
+import argparse
+from argparse import RawTextHelpFormatter
+
+def parseArguments():
+    parser = argparse.ArgumentParser(description='StopAndWait', formatter_class=RawTextHelpFormatter)
+    parser.add_argument("-v", "--version", action='version', version='')
+    parser.add_argument("-ri", "--receiverip", required=True, help="Define receiver ip")
+    parser.add_argument("-rp", "--receiverport", type=int, required=True, help="Define receiver port")
+
+    return parser.parse_args()
+
+# server_socket.settimeout(1)  
+
+def main():
+    args = parseArguments()
+    dest = (args.receiverip, int(args.receiverport))
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        server_socket.bind(dest)
+        print('Server established!')
+        print('Waiting for packages')   
+    except:
+        print('Error establishing server...')
+
         exit()
+    while (True):
+        acknowledged = False
+        while True:
+            data, adress = server_socket.recvfrom(512)
+            number = random.randint(0, 100)
+            if(number > 30 and acknowledged == False):
+                server_socket.sendto(b'Acknowledged', adress)
+                print("Received \'" + str(data, 'utf-8') + "\' with ACK")
+                acknowledged = True
+                break
+            else:
+                # server_socket.sendto(b'AKC', adress)
+                print("Received \'" + str(data, 'utf-8') + "\', but could not send ACK")
+
+if __name__ == "__main__":
+    main()
--- sender.py
+++ sender.py
@@ -1,38 +1,51 @@
 import socket
+import argparse
+from argparse import RawTextHelpFormatter
+
+def parseArguments():
+    parser = argparse.ArgumentParser(description='StopAndWait', formatter_class=RawTextHelpFormatter)
+    parser.add_argument("-v", "--version", action='version', version='')
+    parser.add_argument("-ri", "--receiveripl", required=True, help="Define receiver ip")
+    parser.add_argument("-rp", "--receiverport", type=int, required=True, help="Define receiver port")
+
+    return parser.parse_args()
 
 def frames_divider(msg, n):
     
     for i in range(0, len(msg), n):
         yield msg[i:i+n]
 
-dest = ('127.0.0.1', 9091)
-while True:
-    message = input('Digite sua mensagem:')
-    delimiter = int(input('Tamanho do delimitador:'))
+def main():
+    args = parseArguments()
+    dest = (args.receiverip, int(args.receiverport))
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.settimeout(1)
     
-    acknowledged = False
-    # spam dest until they acknowledge me (sounds like my kids)  
-
-    for frame in frames_divider(message, delimiter):
-        if len(frame) < delimiter:
-            frame = frame.ljust(delimiter, '#')                 
-        my_socket.sendto(bytes(frame, 'utf-8'), dest)
-        try:
-            ACK, address = my_socket.recvfrom(1024)
-            acknowledged = True
-            print("AKNOWLODGED!")
-        except socket.timeout:
-            print("TIME OUT!")
-            my_socket.sendto(bytes(frame, 'utf-8'), dest)
-            acknowledged = False
+    while True:
+        message = input('Digite sua mensagem:')
+        delimiter = int(input('Tamanho do delimitador:'))    
         acknowledged = False
-        # print (ACK)
-    print('\n')
 
-my_socket.close()
+        for frame in frames_divider(message, delimiter):
+            if len(frame) < delimiter:
+                frame = frame.ljust(delimiter, '#')                 
+            my_socket.sendto(bytes(frame, 'utf-8'), dest)
+            while True:
+                try:
+                    ACK = my_socket.recvfrom(1024)
+                    acknowledged = True
+                    print(str(ACK, 'utf-8'))
+                    break
+                except socket.timeout:
+                    print("TIME OUT! Resending frame: " + str(frame))  
+                    my_socket.sendto(bytes(frame, 'utf-8'), dest)
+                    acknowledged = False
+                acknowledged = False
+        print('\n')
+
+    my_socket.close()
 
 
 
-
+if __name__ == "__main__":
+    main()
